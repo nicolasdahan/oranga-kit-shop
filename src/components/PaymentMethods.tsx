f9bbd64e23ac2d706b4a857d5f67fdae147e2267
Nicolas Dahan <nicolasdahan@outlook.com>
2026-01-16 16:55:46 +0100

    update

--- src/components/PaymentMethods.tsx
+++ src/components/PaymentMethods.tsx
@@ -12,56 +12,56 @@ const PaymentMethods = () => {
           </p>
           <div className="flex flex-wrap justify-center items-center gap-6">
             {/* Visa */}
-            <div className="h-8">
+            <div className="w-16 h-10 flex items-center justify-center">
               <img 
                 src="/payment_logo/Visa_2021.svg.png" 
                 alt="Visa" 
-                className="h-full w-auto object-contain"
+                className="max-h-full max-w-full object-contain"
               />
             </div>
 
             {/* Mastercard */}
-            <div className="h-8">
+            <div className="w-16 h-10 flex items-center justify-center">
               <img 
                 src="/payment_logo/Mastercard_2019_logo.svg" 
                 alt="Mastercard" 
-                className="h-full w-auto object-contain"
+                className="max-h-full max-w-full object-contain"
               />
             </div>
 
             {/* PayPal */}
-            <div className="h-8">
+            <div className="w-16 h-10 flex items-center justify-center">
               <img 
                 src="/payment_logo/paypal-3384015_1280.webp" 
                 alt="PayPal" 
-                className="h-full w-auto object-contain"
+                className="max-h-full max-w-full object-contain"
               />
             </div>
 
             {/* Google Pay */}
-            <div className="h-8">
+            <div className="w-16 h-10 flex items-center justify-center">
               <img 
                 src="/payment_logo/Google_Pay_Logo.svg.png" 
                 alt="Google Pay" 
-                className="h-full w-auto object-contain"
+                className="max-h-full max-w-full object-contain"
               />
             </div>
 
             {/* Klarna */}
-            <div className="h-8">
+            <div className="w-16 h-10 flex items-center justify-center">
               <img 
                 src="/payment_logo/klarna_logo_icon_168402.webp" 
                 alt="Klarna" 
-                className="h-full w-auto object-contain"
+                className="max-h-full max-w-full object-contain"
               />
             </div>
 
             {/* Apple Pay */}
-            <div className="h-8">
+            <div className="w-16 h-10 flex items-center justify-center">
               <img 
                 src="/payment_logo/Apple_Pay-Logo.wine.svg" 
                 alt="Apple Pay" 
-                className="h-full w-auto object-contain"
+                className="max-h-full max-w-full object-contain"
               />
             </div>
           </div>
