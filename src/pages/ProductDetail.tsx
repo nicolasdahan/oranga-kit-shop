d9e89a3afb55e528daccc2bbd12cfedac2b05a28
Nicolas Dahan <nicolasdahan@outlook.com>
2026-01-20 11:02:46 +0100

    update paypal 4

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -458,7 +458,7 @@ const ProductDetail = () => {
               <img 
                 src="/payment_logo/PayPal-Yellow-Payment-Button.png" 
                 alt="PayPal Checkout"
-                className="h-full w-auto max-w-full object-contain scale-1450"
+                className="h-full w-auto max-w-full object-contain scale-300"
               />
             </Button>
           </div>
